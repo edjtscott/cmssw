6e00d3e10f0a99cf6e18c9c3ed530157b4e0a8db
Edward Scott <e.scott15@ic.ac.uk>
2017-01-13 14:04:45 +0100

    first attempt at a contiguous multiclustering algorithm that moves away from highest energy layer. Works but some jobs failing

--- RecoLocalCalo/HGCalRecAlgos/src/HGCalDepthPreClusterer.cc
+++ RecoLocalCalo/HGCalRecAlgos/src/HGCalDepthPreClusterer.cc
@@ -42,54 +42,55 @@ std::vector<reco::HGCalMultiCluster> HGCalDepthPreClusterer::makePreClusters(con
       ++used;
       bool doneForward = false;
       unsigned previousLayer = clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[i]]->hitsAndFractions()[0].first);
-      std::cout << "previous layer = " << previousLayer <<  std::endl;
-      std::cout << "entering forward multcluster association" << std::endl;
+      std::vector<int> tempLayerVec;
+      tempLayerVec.push_back(previousLayer);
       while( !doneForward ) {
-        doneForward = true;
 	for(unsigned int j = i+1; j < es.size(); ++j) {
+          doneForward = true;
 	  if(vused[j]==0) {
 	    if( clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[j]]->hitsAndFractions()[0].first) != previousLayer + 1 ) continue;
-            std::cout << "current layer = " << clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[j]]->hitsAndFractions()[0].first) <<  std::endl;
 	    float distanceCheck = 9999.;
 	    if( realSpaceCone ) distanceCheck = distReal(thecls[es[i]],thecls[es[j]]);
 	    else distanceCheck = dist(thecls[es[i]],thecls[es[j]]);
 	    if( distanceCheck<radius && int(thecls[es[i]]->z()*vused[i])>0 ) {
 	      temp.push_back(thecls[es[j]]);
+              tempLayerVec.push_back(previousLayer+1);
 	      vused[j]=vused[i];
 	      ++used;
 	      previousLayer++;
               doneForward = false;
-              break;
 	    }	
 	  }
 	}
       }
-      std::cout << "done forward multcluster association, starting backward" << std::endl;
       bool doneBackward = false;
       previousLayer = clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[i]]->hitsAndFractions()[0].first);
       while( !doneBackward ) {
-        doneBackward = true;
 	for(unsigned int j = i+1; j < es.size(); ++j) {
+          doneBackward = true;
 	  if(vused[j]==0) {
-	    if( clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[j]]->hitsAndFractions()[0].first) != 
-                clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[i]]->hitsAndFractions()[0].first) - 1 ) continue;
+	    if( clusterTools->getRecHitTools().getLayerWithOffset(thecls[es[j]]->hitsAndFractions()[0].first) != previousLayer - 1) continue;
 	    float distanceCheck = 9999.;
 	    if( realSpaceCone ) distanceCheck = distReal(thecls[es[i]],thecls[es[j]]);
 	    else distanceCheck = dist(thecls[es[i]],thecls[es[j]]);
 	    if( distanceCheck<radius && int(thecls[es[i]]->z()*vused[i])>0 ) {
 	      temp.push_back(thecls[es[j]]);
+              tempLayerVec.push_back(previousLayer-1);
 	      vused[j]=vused[i];
 	      ++used;
 	      previousLayer = previousLayer-1;
               doneBackward = false;
-              break;
 	    }	
 	  }
 	}
       }
-      std::cout << "done backward multcluster association" << std::endl;
-      std::cout << "size of multi = " << temp.size() << std::endl;
       if( temp.size() > minClusters ) {
+        if( temp.size() > 2 ) {
+          std::cout << "seed layer = " << tempLayerVec[0] << std::endl;
+          std::sort(tempLayerVec.begin(), tempLayerVec.end());
+          for( uint i=0; i<tempLayerVec.size(); i++ ) std::cout << "layer " << i << "is " << tempLayerVec[i] << std::endl;
+          std::cout << std::endl;
+        }
         thePreClusters.push_back(temp);
         auto& back = thePreClusters.back();
         back.setPosition(clusterTools->getMultiClusterPosition(back));
